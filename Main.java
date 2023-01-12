d089e2d02376513536408ee5b70e4ba61cac428f
hasnainmalik123 <122431655+hasnainmalik123@users.noreply.github.com>
2023-01-12 22:10:12 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,34 +1,5 @@
-import java.util.ArrayList;
-import java.util.Stack;
+package lab14;
 
-interface Banking_System {
-	public void print();
-}
+public class main {
 
-class bill_payment implements Banking_System {
-	public void print() {
-		ArrayList<String> customers = new ArrayList<String>();
-		customers.add("Rameez");
-		customers.add("Raees");
-		customers.add("Ali");
-		System.out.println("\nCustomer List for payment");
-		System.out.println(customers);
-		System.out.println("\nPending customers");
-		customers.remove(2);
-		customers.remove(0);
-		System.out.println(customers);
-	}
-}
-
-abstract class loan implements Banking_System {
-	public void stack_push(Stack<Integer> S) {
-		for (int i = 1; i <= 5; i++) {
-			S.push(i);
-		}
-	}
-
-	public void stack_pop(Stack<Integer> S) {
-		System.out.println("Customer list a/c to least limit extention");
-		int j = 1;
-	}
 }
